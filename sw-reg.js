70cb75d15d749729bd81d9c9f3163f4881299cb5
GANZ-VILMA <csesznok.sandor@ganziskola.hu>
2025-06-30 02:57:01 +0200

    Update sw-reg.js

--- sw-reg.js
+++ sw-reg.js
@@ -1,7 +1,7 @@
 // Service worker regisztrálása (OFFLINE módhoz)
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker.register('/sw.js')
       .then(reg => console.log('Service Worker regisztrálva:', reg.scope))
       .catch(err => console.error('Service Worker hiba:', err));
   });
